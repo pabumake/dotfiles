b69a8f19fffbb12f0f16defe6d3af229a18a8bc5
pabu <pabu@gengar.local>
2026-04-12 16:07:12 +0200

    adjusting terminal behaviour and tweak aliases

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,20 @@
+######################
+# Aliases
+######################
+
+######################
+# Applications
+######################
+# alias code="open -a 'Visual Studio Code - Insiders'"
+
+######################
+# Fast Access Folders
+######################
+# alias pbproj="cd $HOME/Documents/03-coding-projects/"
+
+######################
+# Command Replacements
+######################
+# brew install eza required
+alias ls="eza --icons --group-directories-first -A"
+alias ll="eza --icons --group-directories-first -lA"
